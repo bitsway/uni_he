3a48cdb9b006753e7e9c3693e6e67514d71b2d23
bitsway <bitsway@gmail.com>
2015-11-16 16:30:37 +0600

    test 4 20151116

--- www/static/survey.js
+++ www/static/survey.js
@@ -8,7 +8,7 @@
 //var apipath='http://e2.businesssolutionapps.com/unilever/syncmobile2/';
 //var apipath_image = 'http://e2.businesssolutionapps.com/unilever/';
 
-//var apipath='http://127.0.0.1:8000/unilever/syncmobile2/';
+//var apipath='http://127.0.0.1:8000/unilever/syncmobile3/';
 //var apipath_image = 'http://127.0.0.1:8000/unilever/';
 
 
